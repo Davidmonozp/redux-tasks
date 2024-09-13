c819682f2c0bda94203bdec99ec297e2b76698ee
Davidmonozp <david_es-zu@hotmail.com>
2024-09-13 14:13:29 -0500

    subiendo proyecto

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import './index.css';
+import { TasksList } from './components/TasksList'
+import { TasksForm } from './components/TasksForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <div className="bg-zinc-900 h-screen text-white">
+      
+      <div className='flex items-center justify-center h-full'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<TasksList />} />
+            <Route path='create-task' element={<TasksForm />} />
+            <Route path='edit-task/:id' element={<TasksForm />} />
+          </Routes>
+        </BrowserRouter>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
